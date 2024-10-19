2ee954c443a43af6ad779bf978c1cb2f26441e35
Viktoriia Storozhuk <victoriiastorozhuk@gmail.com>
2024-10-19 12:32:57 +0200

    ColorExtension: add extensions

---
+++ Monify/Extensions/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtension.swift
+//  Monify
+//
+//  Created by temp on 19.10.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBackground = Color("Background")
+    static let customIcon = Color("Icon")
+    static let customText = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
