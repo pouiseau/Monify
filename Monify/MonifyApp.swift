e152b5600c925685ea49b12fc6acdee9f6a96ba0
Viktoriia Storozhuk <victoriiastorozhuk@gmail.com>
2024-10-19 12:08:48 +0200

    Initial Commit

---
+++ Monify/MonifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonifyApp.swift
+//  Monify
+//
+//  Created by temp on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MonifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonifyTests/MonifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonifyTests.swift
+//  MonifyTests
+//
+//  Created by temp on 19.10.2024.
+//
+
+import Testing
+@testable import Monify
+
+struct MonifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
